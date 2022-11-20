01c9b078ee1af3816df7eada67ba98d9dcaded96
Hugo <hugo.cridlig@esme.fr>
2022-11-20 22:11:50 +0100

    bubble sort 40 min

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from geopy import distance
 import numpy as np
 import time
+import sort
 
 def main():
     pass
@@ -22,17 +23,38 @@ def distance_calculate(org_lat, org_long, dest_lat, dest_long):
     #return org_lat + org_long + dest_lat + dest_long
 
 
+def sort_data(arr):
+    
+    n = len(arr)
+    swapped = False
+    
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j][6] > arr[j + 1][6]:
+                swapped = True
+                arr[[j, j+1]] = arr[[j+1, j]]
+                
+        if swapped == False:
+            break
+    
+    return pd.DataFrame(arr, columns=['Code_commune_INSEE', 'Nom_commune', 'Code_postal', 'Ligne_5', 'Libellé_d_acheminement', 'coordonnees_gps', 'Distance',])
+
+        
+
+
+
 if __name__ == '__main__':
     main()
     data = import_data()
     coordinates = data["coordonnees_gps"].loc[data['Nom_commune'] == "PARIS 01"]
 
-    coordinates2 = data["coordonnees_gps"].loc[data['Nom_commune'] == "ST PHILIPPE"].values[0].split(',')
+    #coordinates2 = data["coordonnees_gps"].loc[data['Nom_commune'] == "ST PHILIPPE"].values[0].split(',')
 
     print(tuple(data["coordonnees_gps"].values[0].split(',')))
     data = data[data['coordonnees_gps'].notna()]
     data["Origin"] = coordinates.values[0]
     data[['Origin_lat', 'Origin_long']] = data['Origin'].str.split(',', n=1, expand=True)
+    data = data.drop("Origin", axis=1)
     
     start = time.time()
     print(coordinates.values[0])
@@ -41,3 +63,14 @@ if __name__ == '__main__':
     data["Distance"] =  distance_calculate(data["Origin_lat"].astype(float), data['Origin_long'].astype(float), data['lat2'].astype(float), data['long2'].astype(float))
     print(time.time() - start)
     print(data)
+    
+    data = data.drop("Origin_lat", axis=1)
+    data = data.drop("Origin_long", axis=1)
+    data = data.drop("lat2", axis=1)
+    data = data.drop("long2", axis=1)
+    
+    start = time.time()
+    data = sort_data(data.to_numpy())
+    print(data)
+    print(time.time() - start)
+    
